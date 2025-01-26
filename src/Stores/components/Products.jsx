3932c899e4a410c3464d6c9386673a31066713cb
Bhima Rakesh <154778717+BhimaRakesh@users.noreply.github.com>
2025-01-27 00:51:48 +0530

    e-mart

--- src/Stores/components/Products.jsx
+++ src/Stores/components/Products.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Mobiles from './Mobiles'
-  import Computers from './Computers'
-import Men from './Men'
-import Kitchen from './Kitchen'
-import Furniture from './Furniture'
-import Ac from './Ac'
-import Watches from './Watches'
-import WomenDressing from './WomenDressing'
+import React from "react";
+import Mobiles from "./Mobiles";
+import Computers from "./Computers";
+import Watch from "./Watch";
+import Men from "./Men";
+import Woman from "./Woman";
+import Furniture from "./Furniture";
+import AC from "./AC";
+import Kitchen from "./Kitchen";
 
 const Products = () => {
   return (
     <div>
-  <Mobiles/>
-  <Computers/>
-  <Ac/>
-  <Furniture/>
-  <Kitchen/>
-  <Men/>
-  <Watches/>
-  <WomenDressing/>
+      <Mobiles />
+      <Computers />
+      <Watch />
+      <Men />
+      <Woman />
+      <Furniture />
+      <AC />
+      <Kitchen />
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
