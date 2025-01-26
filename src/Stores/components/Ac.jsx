d0e1a7520f3bb981e87454f712bf132cf71893d7
Bhima Rakesh <154778717+BhimaRakesh@users.noreply.github.com>
2025-01-27 00:41:07 +0530

    Update Ac.jsxe-mart

--- src/Stores/components/Ac.jsx
+++ src/Stores/components/Ac.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import { acData } from '../data/ac'
+import React from "react";
+import { acData } from "../data/ac";
+import { Link } from "react-router-dom";
 
-const Ac = () => {
+const AC = () => {
+  const firstFiveImages = acData.slice(0, 5);
 
-    const firstFiveAC =acData.slice(0,5)
   return (
-   <>
-   AC
-   <div className='proSection'>
-    {
-      
-      firstFiveAC.map((item , index)=>{
-        return(
-          <div className='ImageBox' key={index}>
-          <img className='proImage' src={item.image} alt="AC"/>
-          </div>
-        )
-      })
-    }
-  </div></>
-  )
-}
+    <>
+      <div className="proTitle">
+        <h2>Air Condition</h2>
+      </div>
+      <div className="proSection">
+        {firstFiveImages.map((item) => {
+          return (
+            <div className="imgBox">
+              <Link to='/ac'>
+              <img className="proImage" src={item.image} alt="" />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-export default Ac
+export default AC;
