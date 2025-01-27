6a43b403b5b3b04adf45fb0b782aa74359a6df46
BhimaRakesh <brakeshrakesh5@gmail.com>
2025-01-28 02:12:54 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/e-mart/' : '/', // Adjust the base path for production
-}));
+  base: '/RR-MART/',  // Update this to match your GitHub repository name
+});
+
